46f50938acc95da8f1eb3d5bdba8b08a5e7dcd6a
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-12-27 20:22:16 -0600

    finished video 16

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -8,3 +8,23 @@ describe('anonymous calculator', () => {
     cy.findByTestId(/total/).should('have.text', '3') // make assertion
   })
 })
+describe('authenticated calculator', () => {
+  it('displays the username when logged in, does not display when logged out', () => {
+    // create user
+    cy.createUser().then(user => {
+      // now, log in our user
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
+
+      // assert that we should see username
+      cy.findByTestId('username-display').should('have.text', user.username)
+      // log out user
+      cy.findByText(/logout/i).click()
+      // assert that we should no longer see the username
+      cy.findByTestId('username-display').should('not.exist', user.username)
+    })
+  })
+})
